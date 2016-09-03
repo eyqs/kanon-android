5528b3d9a671e984b21eb60a0b704f07be0494d2
eyqs <eugene@eyqs.ca>
2016-09-02 19:30:48 -0700

    Add tone generator from by Simon Lightfoot

--- app/src/main/java/ca/eyqs/kanon/MainActivity.java
+++ app/src/main/java/ca/eyqs/kanon/MainActivity.java
@@ -1,6 +1,9 @@
 package ca.eyqs.kanon;
 
 import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,5 +25,23 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    /** Made by slightfoot at https://gist.github.com/slightfoot/6330866 */
+    private AudioTrack generateTone(double freqHz, int durationMs)
+    {
+        int count = (int) (44100.0 * 2.0 * (durationMs / 1000.0)) & ~1;
+        short[] samples = new short[count];
+        for (int i = 0; i < count; i += 2) {
+            short sample = (short) (0x7FFF *
+                Math.sin(2 * Math.PI * i / (44100.0 / freqHz)));
+            samples[i + 0] = sample;
+            samples[i + 1] = sample;
+        }
+        AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
+            AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT,
+            count * (Short.SIZE / 8), AudioTrack.MODE_STATIC);
+        track.write(samples, 0, count);
+        return track;
     }
 }
