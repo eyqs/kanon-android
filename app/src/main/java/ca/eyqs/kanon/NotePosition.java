71d4598463bfa52f74f1d9c15107b54b4f400cb1
eyqs <eugene@eyqs.ca>
2016-09-06 18:03:52 -0700

    Draw notes according to staff position

---
+++ app/src/main/java/ca/eyqs/kanon/NotePosition.java
@@ -0,0 +1,16 @@
+package ca.eyqs.kanon;
+
+public class NotePosition {
+    public float x;
+    public float y;
+    public NotePosition(float position, float height) {
+        x = position;
+        y = height;
+    }
+    public float getX() {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+}
--- app/src/main/java/ca/eyqs/kanon/NotesView.java
+++ app/src/main/java/ca/eyqs/kanon/NotesView.java
@@ -9,16 +9,24 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NotesView extends View {
 
     public static final int canvas_width = 80;
     public static final int canvas_height = 202;
     public static final int note_width = 40;
     public static final int note_height = 22;
-    public static double density;
+    public static final int second_offset = 36;
+    public static final int staff_spacing = 10;
+    public static List<NotePosition> notes = new ArrayList<NotePosition>();
+    public static Drawable note;
+    public static float density;
 
     public NotesView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        note = ContextCompat.getDrawable(this.getContext(), R.drawable.note);
         DisplayMetrics dm = new DisplayMetrics();
         WindowManager wm = (WindowManager)
             context.getSystemService(Context.WINDOW_SERVICE);
@@ -26,14 +34,20 @@ public class NotesView extends View {
         density = dm.density;
     }
 
+    public void addNote(int position, int height) {
+        float x = position * second_offset;
+        float y = height * staff_spacing + (canvas_height - note_height) / 2;
+        notes.add(new NotePosition(x, y));
+    }
+
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Drawable note = ContextCompat.getDrawable(
-            this.getContext(), R.drawable.note);
-        int x = 0;
-        int y = (canvas_height - note_height) * (int) (density / 2);
-        note.setBounds(x, y, x + (int) (note_width * density),
-            y + (int) (note_height * density));
-        note.draw(canvas);
+        for (NotePosition np : notes) {
+            note.setBounds((int) (np.getX() * density),
+                (int) (np.getY() * density),
+                (int) ((np.getX() + note_width) * density),
+                (int) ((np.getY() + note_height) * density));
+            note.draw(canvas);
+        }
     }
 }
